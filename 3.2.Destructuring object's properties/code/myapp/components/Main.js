af69745c84d7184d7ccebb129038a9f7a71d0ed7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-12-15 19:20:11 +0700

    update

---
+++ 3.2.Destructuring object's properties/code/myapp/components/Main.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+import {doSomething, sum, do2Tasks} from '../calculations/calculations'
+import Rectangle from '../models/Rectangle'
+
+export default class Main extends Component{
+    componentDidMount() {
+        //doSomething()
+        //alert("6 plus 4 = "+sum(6,4))//press Cmd/Ctrl+S to save and reload
+        //console.log("6 plus 4 = "+sum(6,4))
+        //do2Tasks()
+    }
+    render(){
+        const rect1 = new Rectangle(0, 0, 100, 200)
+        const rect2 = new Rectangle(1, 1, 100, 200)
+        rect1.width = 500
+        const rect3 = {...rect1}
+        //Desctructuring an array         
+        let [a, b, ...rest] = [120, 150, 170, 180, 200] //... = spread operator
+        const [x, y, z = 66] = [5, 7] //z get a "default value"
+        //Destructuring an object
+        let studentA = {name: "Hoang", age: 30, marks: {math: 5, physics: 6}}
+        //studentA is a "nested object"
+        const {name, age} = studentA
+        const {math, physics, english = 5} = studentA.marks //default value
+        return <View>
+            <Text>{JSON.stringify(rect1)}</Text>
+            <Text>{`rect1's key = ${JSON.stringify(Object.keys(rect1))}`}</Text>
+            {/* Clone an object ? */}       
+            <Text>rect3 = {JSON.stringify(rect3)}</Text>     
+            <Text>Distance = {rect1.distance(rect2)}</Text>
+            <Text style={{fontSize: 23}}>
+                a = {a}, b = {b}, rest = {JSON.stringify(rest)}, z = {z}
+                name = {name}, age = {age}, math = {math}, physics = {physics}, english ={english}
+            </Text>
+        </View>
+    }
+}
